38b119f237c9b691fc5cf0affc384545aa4fc69c
CarlosHerrera1210 <ch7690362@gmail.com>
2025-11-11 18:57:01 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ cv-react-carlos-herrera/src/components/CabeceraCV.jsx
@@ -0,0 +1,9 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Carlos Herrera</h1>
+      <h2>Desarrollador Front-End Jr.</h2>
+      <p>📞 3203959625 | ✉️ ch7690362@gmail.com | Medellin, Colombia</p>
+    </header>
+  );
+}
